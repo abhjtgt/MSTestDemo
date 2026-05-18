1fc16f67ad6505932ae8ff4d45304cfe58d51850
abhijit <abhijitdd@yahoo.co.in>
2026-05-18 17:08:20 +0530

    initial commit

---
+++ MathDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math.PI: " + Math.PI);
+            Console.ReadKey();
+        }
+
+
+    }
+}
