deb97c3143f3a8cac9f40edfa5e94be6406ce64a
abhijit <abhijitdd@yahoo.co.in>
2026-05-19 17:55:52 +0530

    Changed project to SDK style

--- MathDemoCode/Properties/AssemblyInfo.cs
+++ MathDemoCode/Properties/AssemblyInfo.cs
@@ -1,15 +1,2 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("MathDemoCode")]
-[assembly: AssemblyDescription("Classic MSTest project for MathDemo.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MathDemoCode")]
-[assembly: AssemblyCopyright("Copyright © 2026")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// SDK-style projects auto-generate assembly attributes.
+// This file is intentionally left mostly empty.
